e6d7ef67a11f022cab63c015e03e8c48e9d220d6
atuliiitbh <127719497+atuliiitbh@users.noreply.github.com>
2023-03-16 16:22:18 +0530

    Create q1_LIS_recursive.cpp

---
+++ dpp_day5/q1_LIS_recursive.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+using namespace std;
+int fun(int arr[],int prev,int i,int n)
+{
+    if(i==n)
+        return 0;
+    if(arr[i]>prev)
+    {
+        return max(1+fun(arr,arr[i],i+1,n),fun(arr,prev,i+1,n));
+    }
+    else
+    {
+        return fun(arr,prev,i+1,n);
+    }
+}
+int main()
+{
+    int arr[] = { 10, 22, 9, 33, 21, 50, 41, 60 };
+    int n=sizeof(arr)/sizeof(arr[0]);
+    int prev=INT_MIN;
+    int index=0;
+    cout<<"Length Of Longest Increasing Subsequence Is: "<<fun(arr,prev,index,n);
+    return 0;
+}
