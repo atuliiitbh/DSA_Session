27983c8d01070137a86bd35c73f4f1ef8216cec3
atuliiitbh <127719497+atuliiitbh@users.noreply.github.com>
2023-03-16 11:22:27 +0530

    Update q1_palindPartion_Recursive.cpp

--- dpp_day4/q1_palindPartion_Recursive.cpp
+++ dpp_day4/q1_palindPartion_Recursive.cpp
@@ -1,4 +1,6 @@
 // Recursive Code Same As MCM !!
+
+// Method 1 Using 2 fn parameters.
 #include<bits/stdc++.h>
 #include <iostream>
 using namespace std;
@@ -35,3 +37,36 @@ int main() {
     cout<<fun(s,i,j);
     return 0;
 }
+// Method 2 using substr and only one parameter:
+/*
+#include<bits/stdc++.h>
+using namespace std;
+bool isPalin(string s)
+{
+    string a=s;
+    reverse(s.begin(),s.end());
+    return s==a;
+}
+int fun(string s)
+{
+    if(s.length()==0 || isPalin(s)==true){return 0;}
+    
+    int cost=INT_MAX;
+    int n=s.length();
+    for(int i=1;i<s.length();i++)
+    {
+        int temp=1+fun(s.substr(0,i))+fun(s.substr(i,n-i));
+        cost=min(temp,cost);
+    }
+    return cost;
+}
+int main()
+{
+    string str = "ababbbabbb";
+    int n=str.length();
+    cout<<fun(str);
+    return 0;    
+}
+
+
+*/
