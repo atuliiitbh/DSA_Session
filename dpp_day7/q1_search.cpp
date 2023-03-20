2882da96537c16f5e44d6fd5b32339cab29f1036
atuliiitbh <127719497+atuliiitbh@users.noreply.github.com>
2023-03-20 09:43:09 +0530

    Create q1_search.cpp

---
+++ dpp_day7/q1_search.cpp
@@ -0,0 +1,11 @@
+// Method 1: Recursive
+class Solution {
+public:
+    TreeNode* searchBST(TreeNode* root, int val) {
+        if(!root){return NULL;}
+        if(root->val==val){return root;}
+        if((root->val)>val){return searchBST(root->left,val);}
+        if((root->val)<val){return searchBST(root->right,val);}
+        return root;
+    }
+};
